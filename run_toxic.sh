e16794cd5e469f6ee882bd34dd42a8e58614aeba
moprei21 <62673995+moprei21@users.noreply.github.com>
2022-11-28 16:17:27 +0100

    update shell scripts

--- run_toxic.sh
+++ run_toxic.sh
@@ -1,20 +1,20 @@
 bash environment.sh
 
-python finetune_germeval.py --model_name_or_path Sahajtomar/German_Zeroshot --output_dir pretrain_out --language de --do_train y --overwrite_output_dir y --filename 8_toxic.csv
+python finetune_germeval.py --model_name_or_path Sahajtomar/German_Zeroshot --output_dir pretrain_out --language de --do_train y --overwrite_output_dir y --filename data/toxic/8_toxic.csv
 python predict_toxic.py --name 8_toxic 
 
 
-python finetune_germeval.py --model_name_or_path Sahajtomar/German_Zeroshot --output_dir pretrain_out --language de --do_train y --overwrite_output_dir y --filename 16_toxic.csv
+python finetune_germeval.py --model_name_or_path Sahajtomar/German_Zeroshot --output_dir pretrain_out --language de --do_train y --overwrite_output_dir y --filename data/toxic/16_toxic.csv
 python predict_toxic.py --name 16_toxic
 
 
-python finetune_germeval.py --model_name_or_path Sahajtomar/German_Zeroshot --output_dir pretrain_out --language de --do_train y --overwrite_output_dir y --filename 32_toxic.csv
+python finetune_germeval.py --model_name_or_path Sahajtomar/German_Zeroshot --output_dir pretrain_out --language de --do_train y --overwrite_output_dir y --filename data/toxic/32_toxic.csv
 python predict_toxic.py --name 32_toxic
 
 
-python finetune_germeval.py --model_name_or_path Sahajtomar/German_Zeroshot --output_dir pretrain_out --language de --do_train y --overwrite_output_dir y --filename 64_toxic.csv
+python finetune_germeval.py --model_name_or_path Sahajtomar/German_Zeroshot --output_dir pretrain_out --language de --do_train y --overwrite_output_dir y --filename data/toxic/64_toxic.csv
 python predict_toxic.py --name 64_toxic
 
 
-python finetune_germeval.py --model_name_or_path Sahajtomar/German_Zeroshot --output_dir pretrain_out --language de --do_train y --overwrite_output_dir y --filename 128_toxic.csv
+python finetune_germeval.py --model_name_or_path Sahajtomar/German_Zeroshot --output_dir pretrain_out --language de --do_train y --overwrite_output_dir y --filename data/toxic/128_toxic.csv
 python predict_toxic.py --name 128_toxic
